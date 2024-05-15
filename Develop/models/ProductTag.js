f748c2c9975b90f1fbae0c1e0a80186048c7f4c9
Patrick Sheehan <patrick@Patricks-MacBook-Air.local>
2024-05-15 12:55:37 -0400

    udpated ProductTag.js

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,6 +6,16 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
     // define columns
   },
   {
